b99e122c6361aaa00051860299e6783d38a59925
Mileup <RaghunathS@hexaware.com>
2022-06-17 18:34:01 +0530

    code changes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import {Avatar} from '@components/avatar';
-import {CloseIcon} from '@components/icons/icons';
-import {Flex} from '@components/layout';
-import {NextLink} from '@components/next-link';
+// import {CloseIcon} from '@components/icons/icons';
+import {Flex,Container} from '@components/layout';
+// import {NextLink} from '@components/next-link';
 import {useAuth} from '@context/auth';
 import Link from 'next/link';
 import {ReactNode, useEffect, useRef, useState} from 'react';
@@ -14,8 +14,11 @@ import {MobileMenuButton, MobileMenuLinks} from './mobile-menu';
 import {useClickOutside} from './use-click-outside';
 import {useLogout} from './use-logout';
 import {UserDropdownButton, UserDropdownLinks} from './user-dropdown';
-
-const commonLinks = [{href: '/', text: 'Home'}];
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+// import "primeicons/primeicons.css";                                //icons
+ 
+const commonLinks = [{href: '/', text: 'Logo'}];
 
 const anonymousDropdownLinks = [
   {
@@ -89,38 +92,17 @@ export function Navbar() {
   return (
     <>
       <div className="bg-gray-50" style={{minHeight: '172px'}}>
-        <nav ref={navbarRef} className="bg-white shadow">
+        <nav ref={navbarRef} className="bg-gray-500 ">
           <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <Flex className="relative justify-between h-16">
-              <Flex className="absolute inset-y-0 left-0 items-center sm:hidden">
-                <MobileMenuButton onClick={toggleMobileMenu}>
-                  {showsMobileMenu ? <CloseIcon /> : <BurgerIcon />}
-                </MobileMenuButton>
-              </Flex>
-              <Flex className="items-center justify-center flex-1 sm:items-stretch sm:justify-start">
-                <Flex className="items-center flex-shrink-0">
-                  <NextLink
-                    href="/"
-                    className="text-gray-900 title-font md:mb-0"
-                    aria-label="home"
-                  >
-                    <BrandIcon />
-                  </NextLink>
-                </Flex>
-                {/* regular navlinks */}
-                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  {links.map(link => (
-                    <ActiveLink
-                      key={link.href}
-                      href={link.href}
-                      activeClassName="border-indigo-500 text-gray-900"
-                      inactiveClassName="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                    >
-                      <a className="inline-flex items-center px-1 pt-1 text-base font-medium border-b-2">
-                        {link.text}
-                      </a>
-                    </ActiveLink>
-                  ))}
+              
+              <Flex className="items-center justify-start">
+              
+                <div className="font-bold border-3 border-round border-bluegray-600 text-900 sm:ml-6 sm:flex sm:space-x-8">
+                
+              Logo
+                    
+                    
                 </div>
               </Flex>
               <Flex className="absolute inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -164,7 +146,7 @@ export function Navbar() {
             </Flex>
           </div>
 
-          <MobileMenuLinks
+          {/* <MobileMenuLinks
             className={`${showsMobileMenu ? 'block' : 'hidden'} sm:hidden`}
           >
             <div className="pt-2 pb-4 space-y-1">
@@ -185,7 +167,7 @@ export function Navbar() {
                 </ActiveLink>
               ))}
             </div>
-          </MobileMenuLinks>
+          </MobileMenuLinks> */}
         </nav>
       </div>
     </>
